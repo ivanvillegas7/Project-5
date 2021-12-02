5fe4992f5ad2b224bdce97a6a12ebabff957af2a
JulianElToro <89639454+JulianElToro@users.noreply.github.com>
2021-12-02 16:02:00 +0100

    Add files via upload

--- DSlit.cpp
+++ DSlit.cpp
@@ -1,14 +1,20 @@
 #include "DSlit.hpp"
 
-DSlit::DSlit(double T_in, int M_in, double v0_in, double h_in, cx_double dt_in) {
+DSlit::DSlit(double T_in, int M_in, double v0_in, double h_in, cx_double dt_in, double xc_in, double sx_in, double px_in, double yc_in, double sy_in, double py_in) {
 
-    //We assign the introduced values to the member variables
+	//We assign the introduced values to the member variables
 
-    T_ = T_in;
-    M_ = M_in;
-    v0_ = v0_in;
-    h_ = h_in;
-    dt_ = dt_in;
+	T_ = T_in;
+	M_ = M_in;
+	v0_ = v0_in;
+	h_ = h_in;
+	dt_ = dt_in;
+	xc_ = xc_in;
+	sx_ = sx_in;
+	px_ = px_in;
+	yc_ = yc_in;
+	sy_ = sy_in;
+	py_ = py_in;
 
 }
 
@@ -16,39 +22,37 @@ DSlit::DSlit(double T_in, int M_in, double v0_in, double h_in, cx_double dt_in) 
 
 void DSlit::create_V(cx_mat& V, double w, double s, double a, double xpos) {
 
-        V.zeros(M_-2, M_-2);
+	V.zeros(M_ - 2, M_ - 2);
 
-        int W = (int)round( (M_-3) * w / 2 );
+	int W = (int)round((M_ - 3) * w / 2);
 
-        int S = (int)round( (M_-3) * s / 2 );
+	int S = (int)round((M_ - 3) * s / 2);
 
-        int A = (int)round( (M_-3) * a );
+	int A = (int)round((M_ - 3) * a);
 
-        int XPOS = (int)round( (M_-3) * xpos );
+	int XPOS = (int)round((M_ - 3) * xpos);
 
-        for(int j = (XPOS - W); j <= (XPOS + W); j++){
+	for (int j = (XPOS - W); j <= (XPOS + W); j++) {
 
-                for(int i = 0; i < (XPOS - (S+A)); i++){
+		for (int i = 0; i < (XPOS - (S + A)); i++) {
 
-                        V(i,j) = v0_;
+			V(i, j) = v0_;
 
-                }
-
-
-                for(int i = (XPOS - S); i <= (XPOS + S); i++){
-
-                        V(i,j) = v0_;
-
-                }
-
-
-                for(int i = (XPOS + (S+A) + 1); i < (M_-2); i++){
-
-                        V(i,j) = v0_;
-			
 		}
 
-        }
+		for (int i = (XPOS - S); i <= (XPOS + S); i++) {
+
+			V(i, j) = v0_;
+
+		}
+
+		for (int i = (XPOS + (S + A) + 1); i < (M_ - 2); i++) {
+
+			V(i, j) = v0_;
+
+		}
+
+	}
 
 }
 
@@ -56,14 +60,14 @@ void DSlit::create_V(cx_mat& V, double w, double s, double a, double xpos) {
 
 
 
-void DSlit::create_AB(sp_cx_mat& A , sp_cx_mat& B, cx_mat V) {
+void DSlit::create_AB(sp_cx_mat& A, sp_cx_mat& B, cx_mat V) {
 
 	int N = (M_ - 2) * (M_ - 2);
 
 	int k;
 
 	cx_double r = 1.0 * 1.0i * dt_ / (2 * h_ * h_);
-	
+
 	cx_vec a(N);
 
 	cx_vec b(N);
@@ -91,27 +95,27 @@ void DSlit::create_AB(sp_cx_mat& A , sp_cx_mat& B, cx_mat V) {
 	vecr2.fill(r);
 
 
-	sp_cx_mat A1(M_ - 2, M_ - 2 );
+	sp_cx_mat A1(M_ - 2, M_ - 2);
 	A1.diag(1) = -vecr;
 	A1.diag(-1) = -vecr;
 
 	sp_cx_mat A2(M_ - 2, M_ - 2);
-	A2.diag(0) = - vecr2;
+	A2.diag(0) = -vecr2;
 
 	sp_cx_mat A3(M_ - 2, M_ - 2);
-	
+
 
 	sp_cx_mat Aa = join_horiz(A1, A2, A3);
-	sp_cx_mat Ab= join_horiz(A2, A1, A2);
-	sp_cx_mat Ac= join_horiz(A3, A2, A1);
+	sp_cx_mat Ab = join_horiz(A2, A1, A2);
+	sp_cx_mat Ac = join_horiz(A3, A2, A1);
 
 	A = join_vert(Aa, Ab, Ac);
 	B = -1.0 * join_vert(Aa, Ab, Ac);
 
 	A.diag(0) = a;
-	
+
 	B.diag(0) = b;
-	
+
 
 }
 
@@ -124,35 +128,54 @@ void DSlit::evolve_t(sp_cx_mat A, sp_cx_mat B, cx_vec& u) {
 
 }
 
+cx_double DSlit::probability(cx_vec& u) {
+
+	cx_double p = 0.0;
+
+	int k;
+
+	for (int i = 0; i < M_ - 2; i++) {
+
+		for (int j = 0; j < M_ - 2; j++) {
+
+			k = i + j * (M_ - 1);
+
+			p += (std::conj(u(k)) * u(k));
 
 
 
-void DSlit::initial_state(sp_cx_vec& u){
+		}
 
-        //AquÃ­ pondremos la funciÃ³n que crea el estado inicial xd
+	}
+
+	return(p);
 
 }
 
+void DSlit::initial_state(cx_vec& u) {
+
+	//Aquí pondremos la función que crea el estado inicial xd, no da risa
+
+	vec x = linspace(0, 1, M_ - 1);
+	vec y = linspace(0, 1, M_ - 1);
+
+	for (int i = 0; i < M_ - 1; i++) {
+
+		for (int j = 0; j < M_ - 1; j++) {
+
+			u(i, j) = exp(-(x(i) - xc_) * (x(i) - xc_) / (2 * sx_ * sx_) - (y(j) - yc_) * (y(j) - yc_) / (2 * sy_ * sy_) + 1.0i * px_ * (x(i) - xc_) + 1.0i * py_ * (y(j) - yc_));
+
+		}
+
+	}
+
+	if (roundf(norm((probability(u) * 100) / 100)) == 1 ){
+
+	
+			
+	}
 
 
-
-double DSlit::probability(sp_cx_vec& u){
-
-        double p = 0.0;
-
-        for (int i = 0; i < M_ - 2; i++) {
-
-                for (int j = 0; j < M_ - 2; j++) {
-
-                        k = i + j * (M_ - 1);
-
-                        p += ( conj(u(K)) * u(k) );
-
-
-
-                }
-
-        }
 
 
 }
--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 
 
 int main(){
-	
-	DSlit my_system(10, 5, 100000, 0.1, 0.1);
 
+	vec SP;
+	SP.load("Simulation Parameters.txt", raw_ascii);
+
+
+	DSlit my_system( SP(2) , 1/SP(0), SP(9), SP(0), SP(1), SP(3), SP(4), SP(5), SP(6), SP(7), SP(8));
+	 
 	int N = (my_system.M_ - 2) * (my_system.M_ - 2);
 
 	sp_cx_mat A(N, N);
@@ -13,8 +17,10 @@ int main(){
 	
 	cx_mat V(my_system.M_ - 2, my_system.M_ - 2);
 
-	my_system.create_V(V);
+	my_system.create_V(V , SP(10), SP(11), SP(12), SP(13));
 
 	my_system.create_AB(A, B , V);
+
+	return 0;
 	
 }
