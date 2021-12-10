9756b269e16bf5cd67125ca27c9b21f3d7f29c16
Elena Pujante Martínez <89639906+elenapuj@users.noreply.github.com>
2021-12-10 16:58:25 +0100

    Add files via upload

--- Include/DSlit.hpp
+++ Include/DSlit.hpp
@@ -24,7 +24,7 @@ public:
 
 	double h_;
 
-	cx_double dt_;
+	double dt_;
 
 	double xc_;
 
@@ -42,13 +42,13 @@ public:
 
 	//Then, we declare also the constructor
 
-	DSlit(double T_in, int M_in, double v0_in, double h_in, cx_double dt_in, double xc_in, double sx_in, double px_in, double yc_in, double sy_in, double py_in);
+	DSlit(double T_in, int M_in, double v0_in, double h_in, double dt_in, double xc_in, double sx_in, double px_in, double yc_in, double sy_in, double py_in);
 
 
 
 	//Finally, we declare some methods for calculating some important things
 
-	void create_V(cx_mat& V, double w, double s, double a, double xpos);
+	void create_V(cx_mat& V, int n, double w, double s, double a, double xpos);
 
 	void create_AB(sp_cx_mat& A, sp_cx_mat& B, cx_mat V);
 
@@ -56,7 +56,7 @@ public:
 
 	void initial_state(cx_vec& u);
 
-	cx_double probability(cx_vec& u);
+	cx_double probability(cx_vec u);
 
 };
 #endif
