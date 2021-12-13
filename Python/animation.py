f97dc5ecd6c11d7e287093564fede7faf5e073a0
mariaperez01 <89605350+mariaperez01@users.noreply.github.com>
2021-12-13 18:39:03 +0100

    Update animation.py

--- Python/animation.py
+++ Python/animation.py
@@ -4,40 +4,20 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import pyarma as pa
 
+# We create a cube, and fill it with a txt that contains the data of the real part
 Re = pa.cube()
-
 Re.load("Colourmap_re.txt")
 
-
-
-#
-# Let's generate a dummy time series for a function z(x,y,t)
-#
-# Set up a 2D xy 
-
+# Set up a 2D xy grid
 h = 0.005
-
 x_points = np.arange(0, 1, h)
-
 y_points = np.arange(0, 1, h)
-
 x, y = np.meshgrid(x_points, y_points, sparse = True)
 
+
 # Array of time points
-
 dt = 2.5e-5
-
 t_points = np.arange(0, 1, dt)
-
-# A function for a Gaussian that is travelling 
-# in the x direction and broadening as time passes
-
-
-#
-# Now the list z_data_list contains a series of "frames" of z(x,y,t), 
-# where each frame can be plotted as a 2D image using imshow. Let's
-# animate it!
-#
 
 # Some settings
 fontsize = 12
@@ -49,11 +29,10 @@ y_min, y_max = y_points[0], y_points[-1]
 fig = plt.figure()
 ax = plt.gca()
 
-
+# We take the 0 slice of the cube
 Re0 = Re[ pa.single_slice , 0 ]
 
-
-# Create a colour scale normalization according to the max z value in the first frame
+# Create a colour scale normalization according to the max Re0 value in the first frame
 norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=Re0.max() )
 
 # Plot the first frame
@@ -74,13 +53,13 @@ cbar.ax.tick_params(labelsize=fontsize)
 time_txt = plt.text(0.95, 0.95, "t = {:.3e}".format(t_min), color="white", 
                     horizontalalignment="right", verticalalignment="top", fontsize=fontsize)
 
-# Function that takes care of updating the z data and other things for each frame
+# Function that takes care of updating the Re0 data and other things for each frame
 def animation(i):
     # Normalize the colour scale to the current frame?
     norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=Re[pa.single_slice, i].max())
     img.set_norm(norm)
 
-    # Update z data
+    # Update Re data
     img.set_data(Re[pa.single_slice, i])
 
     # Update the time label
@@ -92,31 +71,21 @@ def animation(i):
 # Use matplotlib.animation.FuncAnimation to put it all together
 anim = FuncAnimation(fig, animation, interval=1, frames=np.arange(0, 81, 2), repeat=False, blit=0)
 
-# Run the animation!
-#plt.show()
-
-# # Save the animation (either with .mp4 or .gif)
+# Save the animation (either with .mp4 or .gif)
 anim.save('Re_animation.mp4', writer="ffmpeg", bitrate=-1, fps=30)
 
-
-
-
-# Imaginary part
-
+# We create a cube, and fill it with a txt that contains the data of the imaginary part
 Im = pa.cube()
-
 Im.load("Colourmap_im.txt" )
 
 # Create figure
 fig = plt.figure()
 ax = plt.gca()
 
-
+# We take the 0 slice of the cube
 Im0 = Im[ pa.single_slice , 0 ]
 
-
-
-# Create a colour scale normalization according to the max z value in the first frame
+# Create a colour scale normalization according to the max Im0 value in the first frame
 norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=Im0.max() )
 
 # Plot the first frame
@@ -137,13 +106,13 @@ cbar.ax.tick_params(labelsize=fontsize)
 time_txt = plt.text(0.95, 0.95, "t = {:.3e}".format(t_min), color="white", 
                     horizontalalignment="right", verticalalignment="top", fontsize=fontsize)
 
-# Function that takes care of updating the z data and other things for each frame
+# Function that takes care of updating the Im0 data and other things for each frame
 def animation(i):
     # Normalize the colour scale to the current frame?
     norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=Im[pa.single_slice, i].max())
     img.set_norm(norm)
 
-    # Update z data
+    # Update Im0 data
     img.set_data(Im[pa.single_slice, i])
 
     # Update the time label
@@ -155,31 +124,21 @@ def animation(i):
 # Use matplotlib.animation.FuncAnimation to put it all together
 anim = FuncAnimation(fig, animation, interval=1, frames=np.arange(0, 81, 2), repeat=False, blit=0)
 
-# Run the animation!
-#plt.show()
-
-# # Save the animation (either with .mp4 or .gif)
+# Save the animation (either with .mp4 or .gif)
 anim.save('Im_animation.mp4', writer="ffmpeg", bitrate=-1, fps=30)
 
-
-
-
-#Probability
-
-# 1 slit
-
+# We create a cube, and fill it with a txt that contains the data of the probability function for 1 slit
 p = pa.cube()
-
 p.load("Colourmap_p_1slit.txt" )
 
 # Create figure
 fig = plt.figure()
 ax = plt.gca()
 
+# We take the 0 slice of the cube
 p0 = p[ pa.single_slice , 0 ]
 
-
-# Create a colour scale normalization according to the max z value in the first frame
+# Create a colour scale normalization according to the max p0 value in the first frame
 norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=p0.max() )
 
 # Plot the first frame
@@ -196,18 +155,17 @@ cbar = fig.colorbar(img, ax=ax)
 cbar.set_label("p", fontsize=fontsize)
 cbar.ax.tick_params(labelsize=fontsize)
 
-
 # Add a text element showing the time
 time_txt = plt.text(0.95, 0.95, "t = {:.3e}".format(t_min), color="white", 
                     horizontalalignment="right", verticalalignment="top", fontsize=fontsize)
 
-# Function that takes care of updating the z data and other things for each frame
+# Function that takes care of updating the p0 data and other things for each frame
 def animation(i):
     # Normalize the colour scale to the current frame?
     norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=p[pa.single_slice, i].max())
     img.set_norm(norm)
 
-    # Update z data
+    # Update p0 data
     img.set_data(p[pa.single_slice, i])
 
     # Update the time label
@@ -219,27 +177,21 @@ def animation(i):
 # Use matplotlib.animation.FuncAnimation to put it all together
 anim = FuncAnimation(fig, animation, interval=1, frames=np.arange(0, 81, 2), repeat=False, blit=0)
 
-# Run the animation!
-#plt.show()
-
-# # Save the animation (either with .mp4 or .gif)
+# Save the animation (either with .mp4 or .gif)
 anim.save('p_1slit_animation.gif', writer="ffmpeg", bitrate=-1, fps=30)
 
-
-# 2 slits
-
+# We create a cube, and fill it with a txt that contains the data of the probability function for 2 slits
 p = pa.cube()
-
 p.load("Colourmap_p_2slits.txt" )
 
 # Create figure
 fig = plt.figure()
 ax = plt.gca()
 
+# We take the 0 slice of the cube
 p0 = p[ pa.single_slice , 0 ]
 
-
-# Create a colour scale normalization according to the max z value in the first frame
+# Create a colour scale normalization according to the max p0 value in the first frame
 norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=p0.max() )
 
 # Plot the first frame
@@ -256,18 +208,17 @@ cbar = fig.colorbar(img, ax=ax)
 cbar.set_label("p", fontsize=fontsize)
 cbar.ax.tick_params(labelsize=fontsize)
 
-
 # Add a text element showing the time
 time_txt = plt.text(0.95, 0.95, "t = {:.3e}".format(t_min), color="white", 
                     horizontalalignment="right", verticalalignment="top", fontsize=fontsize)
 
-# Function that takes care of updating the z data and other things for each frame
+# Function that takes care of updating the p0 data and other things for each frame
 def animation(i):
     # Normalize the colour scale to the current frame?
     norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=p[pa.single_slice, i].max())
     img.set_norm(norm)
 
-    # Update z data
+    # Update p0 data
     img.set_data(p[pa.single_slice, i])
 
     # Update the time label
@@ -279,27 +230,21 @@ def animation(i):
 # Use matplotlib.animation.FuncAnimation to put it all together
 anim = FuncAnimation(fig, animation, interval=1, frames=np.arange(0, 81, 2), repeat=False, blit=0)
 
-# Run the animation!
-#plt.show()
-
-# # Save the animation (either with .mp4 or .gif)
+# Save the animation (either with .mp4 or .gif)
 anim.save('p_2slits_animation.gif', writer="ffmpeg", bitrate=-1, fps=30)
 
-
-# 3 slits
-
+# We create a cube, and fill it with a txt that contains the data of the probability function for 3 slits
 p = pa.cube()
-
 p.load("Colourmap_p_3slits.txt" )
 
 # Create figure
 fig = plt.figure()
 ax = plt.gca()
 
+# We take the 0 slice of the cube
 p0 = p[ pa.single_slice , 0 ]
 
-
-# Create a colour scale normalization according to the max z value in the first frame
+# Create a colour scale normalization according to the max p0 value in the first frame
 norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=p0.max() )
 
 # Plot the first frame
@@ -316,18 +261,17 @@ cbar = fig.colorbar(img, ax=ax)
 cbar.set_label("p", fontsize=fontsize)
 cbar.ax.tick_params(labelsize=fontsize)
 
-
 # Add a text element showing the time
 time_txt = plt.text(0.95, 0.95, "t = {:.3e}".format(t_min), color="white", 
                     horizontalalignment="right", verticalalignment="top", fontsize=fontsize)
 
-# Function that takes care of updating the z data and other things for each frame
+# Function that takes care of updating the p0 data and other things for each frame
 def animation(i):
     # Normalize the colour scale to the current frame?
     norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=p[pa.single_slice, i].max())
     img.set_norm(norm)
 
-    # Update z data
+    # Update p0 data
     img.set_data(p[pa.single_slice, i])
 
     # Update the time label
@@ -339,8 +283,5 @@ def animation(i):
 # Use matplotlib.animation.FuncAnimation to put it all together
 anim = FuncAnimation(fig, animation, interval=1, frames=np.arange(0, 81, 2), repeat=False, blit=0)
 
-# Run the animation!
-#plt.show()
-
-# # Save the animation (either with .mp4 or .gif)
+# Save the animation (either with .mp4 or .gif)
 anim.save('p_3slits_animation.gif', writer="ffmpeg", bitrate=-1, fps=30)
